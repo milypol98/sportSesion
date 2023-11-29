ba5c139c7179ad3fc116361cc47aaf1ec427bf6b
milypol98 <milypol98@gmail.com>
2023-11-29 15:31:12 +0100

    Pierwszy commit

---
+++ src/main/java/pl/milypol/sportSesion/SportSesionApplication.java
@@ -0,0 +1,13 @@
+package pl.milypol.sportSesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportSesionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportSesionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/milypol/sportSesion/SportSesionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milypol.sportSesion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportSesionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
